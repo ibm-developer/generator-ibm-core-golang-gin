0da98b66d7f6e7f7bfb91a4c18de6da7261f13fd
Michael Blume <mablume97@gmail.com>
2018-08-01 10:39:14 -0500

    chore: refactored tabs into spaces for eslint

--- test/common/generator.defaults.test.js
+++ test/common/generator.defaults.test.js
@@ -23,109 +23,109 @@ const myHelpers = require('../../lib/helpers.js');
 const PROJECT_NAME = 'ProjectName';
 
 describe('Go Generator integration test for default when application type not specified', () => {
-	beforeEach(() => {
+  beforeEach(() => {
 
-		// Mock the options, set up an output folder and run the generator
-		return helpers
-			.run(path.join(__dirname, '../../generators/app'))
-			.withOptions({
-				bluemix: JSON.stringify({
-					name: PROJECT_NAME,
-				})
-			})
-			.toPromise(); // Get a Promise back when the generator finishes
-	});
+    // Mock the options, set up an output folder and run the generator
+    return helpers
+      .run(path.join(__dirname, '../../generators/app'))
+      .withOptions({
+        bluemix: JSON.stringify({
+          name: PROJECT_NAME,
+        })
+      })
+      .toPromise(); // Get a Promise back when the generator finishes
+  });
 
-	describe('basic file structure for any project type test', () => {
-		const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
+  describe('basic file structure for any project type test', () => {
+    const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe(common.commonFile.server_go, () => {
-		it('contains default blank message', () => {
-			assert.fileContent(common.commonFile.server_go, 'c.String(http.StatusOK, "You are now running a blank Go application")')
-		});
-	});
+  describe(common.commonFile.server_go, () => {
+    it('contains default blank message', () => {
+      assert.fileContent(common.commonFile.server_go, 'c.String(http.StatusOK, "You are now running a blank Go application")')
+    });
+  });
 
-	describe(common.commonFile.README_md, () => {
-		it('contains default project name', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				myHelpers.sanitizeAppName(PROJECT_NAME)
-			);
-		});
+  describe(common.commonFile.README_md, () => {
+    it('contains default project name', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        myHelpers.sanitizeAppName(PROJECT_NAME)
+      );
+    });
 
-		it('contains IBM Cloud badge', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				'[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
-			);
-		});
-	});
+    it('contains IBM Cloud badge', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        '[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
+      );
+    });
+  });
 
-	describe(common.commonFile.gitignore, () => {
-		it('contains .DS_Store', () => {
-			assert.fileContent(common.commonFile.gitignore, '.DS_Store');
-		});
-	});
+  describe(common.commonFile.gitignore, () => {
+    it('contains .DS_Store', () => {
+      assert.fileContent(common.commonFile.gitignore, '.DS_Store');
+    });
+  });
 });
 
 describe('Go Generator integration test for webapp', () => {
-	beforeEach(() => {
+  beforeEach(() => {
 
-		// Mock the options, set up an output folder and run the generator
-		return helpers
-			.run(path.join(__dirname, '../../generators/app'))
-			.withOptions({
-				bluemix: JSON.stringify({
-					name: PROJECT_NAME
-				}),
-				spec: JSON.stringify({
-					applicationType: 'WEBAPP'
-				})
-			})
-			.toPromise(); // Get a Promise back when the generator finishes
-	});
+    // Mock the options, set up an output folder and run the generator
+    return helpers
+      .run(path.join(__dirname, '../../generators/app'))
+      .withOptions({
+        bluemix: JSON.stringify({
+          name: PROJECT_NAME
+        }),
+        spec: JSON.stringify({
+          applicationType: 'WEBAPP'
+        })
+      })
+      .toPromise(); // Get a Promise back when the generator finishes
+  });
 
 
-	describe('basic file structure for any project type test', () => {
-		const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
+  describe('basic file structure for any project type test', () => {
+    const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe('basic file structure for any webapp framework test', () => {
-		const expected = Object.keys(common.webAppFile).map(key => common.webAppFile[key]);
+  describe('basic file structure for any webapp framework test', () => {
+    const expected = Object.keys(common.webAppFile).map(key => common.webAppFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe(common.commonFile.README_md, () => {
-		it('contains default project name', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				myHelpers.sanitizeAppName(PROJECT_NAME)
-			);
-		});
+  describe(common.commonFile.README_md, () => {
+    it('contains default project name', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        myHelpers.sanitizeAppName(PROJECT_NAME)
+      );
+    });
 
-		it('contains IBM Cloud badge', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				'[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
-			);
-		});
-	});
+    it('contains IBM Cloud badge', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        '[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
+      );
+    });
+  });
 
-	describe(common.commonFile.gitignore, () => {
-		it('contains .DS_Store', () => {
-			assert.fileContent(common.commonFile.gitignore, '.DS_Store');
-		});
-	});
+  describe(common.commonFile.gitignore, () => {
+    it('contains .DS_Store', () => {
+      assert.fileContent(common.commonFile.gitignore, '.DS_Store');
+    });
+  });
 });
--- test/common/generator.prompts.test.js
+++ test/common/generator.prompts.test.js
@@ -26,185 +26,185 @@ const fs = require('fs');
 let PROJECT_NAME = '';
 let appPath = ''
 do{
-	PROJECT_NAME = 'ProjectName' + Math.random().toString(36).substring(12)
-	appPath = path.join(
-		process.env.GOPATH || '',
-		'src/',
-		myHelpers.sanitizeAppName(PROJECT_NAME)
-	);
+  PROJECT_NAME = 'ProjectName' + Math.random().toString(36).substring(12)
+  appPath = path.join(
+    process.env.GOPATH || '',
+    'src/',
+    myHelpers.sanitizeAppName(PROJECT_NAME)
+  );
 }while(fs.existsSync(appPath))
 
 function clean() {
-	if (!process.env.GOPATH) return;
-	let exec = require('child_process').exec;
-	exec('rm -r ' + appPath);
+  if (!process.env.GOPATH) return;
+  let exec = require('child_process').exec;
+  exec('rm -r ' + appPath);
 }
 
 describe('Generates a web app through prompts', () => {
-	beforeEach(() => {
-		// Mock the options, set up an output folder and run the generator
-		return (
-			helpers
-				.run(path.join(__dirname, '../../generators/app'))
-				.withPrompts({
-					appName: PROJECT_NAME,
-					applicationType: 'WEBAPP'
-				})
-				.toPromise()
-		); // Get a Promise back when the generator finishes
-	});
+  beforeEach(() => {
+    // Mock the options, set up an output folder and run the generator
+    return (
+      helpers
+        .run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          appName: PROJECT_NAME,
+          applicationType: 'WEBAPP'
+        })
+        .toPromise()
+    ); // Get a Promise back when the generator finishes
+  });
 
-	afterEach(() => {
-		clean();
-	});
+  afterEach(() => {
+    clean();
+  });
 
-	describe('basic file structure for any project type test', () => {
-		const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
+  describe('basic file structure for any project type test', () => {
+    const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe('basic file structure for webapp test', () => {
-		const expected = Object.keys(common.webAppFile).map(key => common.webAppFile[key]);
+  describe('basic file structure for webapp test', () => {
+    const expected = Object.keys(common.webAppFile).map(key => common.webAppFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe(common.commonFile.README_md, () => {
-		it('contains default project name', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				myHelpers.sanitizeAppName(PROJECT_NAME)
-			);
-		});
+  describe(common.commonFile.README_md, () => {
+    it('contains default project name', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        myHelpers.sanitizeAppName(PROJECT_NAME)
+      );
+    });
 
-		it('contains IBM Cloud badge', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				'[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
-			);
-		});
-	});
+    it('contains IBM Cloud badge', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        '[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
+      );
+    });
+  });
 
-	describe(common.commonFile.gitignore, () => {
-		it('contains .DS_Store', () => {
-			assert.fileContent(common.commonFile.gitignore, '.DS_Store');
-		});
-	});
+  describe(common.commonFile.gitignore, () => {
+    it('contains .DS_Store', () => {
+      assert.fileContent(common.commonFile.gitignore, '.DS_Store');
+    });
+  });
 
-	describe('Go file structure', () => {
-		it('generates the directory in the Gopath', () => {
-			if (process.env.GOPATH) {
-				assert(fs.existsSync(appPath));
-			}
-		});
-	});
+  describe('Go file structure', () => {
+    it('generates the directory in the Gopath', () => {
+      if (process.env.GOPATH) {
+        assert(fs.existsSync(appPath));
+      }
+    });
+  });
 });
 
 
 describe('Generates microservice app through prompts', () => {
-	beforeEach(() => {
-		if (process.env.GOPATH) {
-			let exec = require('child_process').exec;
-			exec('rm -r ' + appPath);
-		}
-		// Mock the options, set up an output folder and run the generator
-		return helpers
-			.run(path.join(__dirname, '../../generators/app'))
-			.withPrompts({
-				appName: PROJECT_NAME,
-				applicationType: 'MS'
-			})
-			.toPromise(); // Get a Promise back when the generator finishes
-	});
+  beforeEach(() => {
+    if (process.env.GOPATH) {
+      let exec = require('child_process').exec;
+      exec('rm -r ' + appPath);
+    }
+    // Mock the options, set up an output folder and run the generator
+    return helpers
+      .run(path.join(__dirname, '../../generators/app'))
+      .withPrompts({
+        appName: PROJECT_NAME,
+        applicationType: 'MS'
+      })
+      .toPromise(); // Get a Promise back when the generator finishes
+  });
 
-	afterEach(() => {
-		clean();
-	});
+  afterEach(() => {
+    clean();
+  });
 
-	describe('basic file structure for any project type test', () => {
-		const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
+  describe('basic file structure for any project type test', () => {
+    const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe('basic file structure for microservice', () => {
-		const expected = Object.keys(common.microserviceFile).map(
-			key => common.microserviceFile[key]
-		);
+  describe('basic file structure for microservice', () => {
+    const expected = Object.keys(common.microserviceFile).map(
+      key => common.microserviceFile[key]
+    );
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 });
 
 describe('Generates blank app with no swagger file through prompts', () => {
-	beforeEach(() => {
-		// Mock the options, set up an output folder and run the generator
-		return (
-			helpers
-				.run(path.join(__dirname, '../../generators/app'))
-				.withPrompts({
-					appName: PROJECT_NAME,
-					applicationType: 'BLANK',
-					useSwagger: false
-				})
-				.toPromise() // Get a Promise back when the generator finishes
-		);
-	});
+  beforeEach(() => {
+    // Mock the options, set up an output folder and run the generator
+    return (
+      helpers
+        .run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          appName: PROJECT_NAME,
+          applicationType: 'BLANK',
+          useSwagger: false
+        })
+        .toPromise() // Get a Promise back when the generator finishes
+    );
+  });
 
-	afterEach(() => {
-		clean();
-	});
+  afterEach(() => {
+    clean();
+  });
 
-	describe('basic file structure for any project type test', () => {
-		const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
+  describe('basic file structure for any project type test', () => {
+    const expected = Object.keys(common.commonFile).map(key => common.commonFile[key]);
 
-		it('generates the expected application files', () => {
-			assert.file(expected);
-		});
-	});
+    it('generates the expected application files', () => {
+      assert.file(expected);
+    });
+  });
 
-	describe(common.commonFile.server_go, () => {
-		it('contains default blank message', () => {
-			assert.fileContent(common.commonFile.server_go, 'c.String(http.StatusOK, "You are now running a blank Go application")')
-		});
-	});
+  describe(common.commonFile.server_go, () => {
+    it('contains default blank message', () => {
+      assert.fileContent(common.commonFile.server_go, 'c.String(http.StatusOK, "You are now running a blank Go application")')
+    });
+  });
 
-	describe(common.commonFile.README_md, () => {
-		it('contains default project name', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				myHelpers.sanitizeAppName(PROJECT_NAME)
-			);
-		});
+  describe(common.commonFile.README_md, () => {
+    it('contains default project name', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        myHelpers.sanitizeAppName(PROJECT_NAME)
+      );
+    });
 
-		it('contains IBM Cloud badge', () => {
-			assert.fileContent(
-				common.commonFile.README_md,
-				'[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
-			);
-		});
-	});
+    it('contains IBM Cloud badge', () => {
+      assert.fileContent(
+        common.commonFile.README_md,
+        '[![](https://img.shields.io/badge/IBM%20Cloud-powered-blue.svg)](https://bluemix.net)'
+      );
+    });
+  });
 
-	describe(common.commonFile.gitignore, () => {
-		it('contains .DS_Store', () => {
-			assert.fileContent(common.commonFile.gitignore, '.DS_Store');
-		});
-	});
+  describe(common.commonFile.gitignore, () => {
+    it('contains .DS_Store', () => {
+      assert.fileContent(common.commonFile.gitignore, '.DS_Store');
+    });
+  });
 
-	describe('Go file structure', () => {
-		it('generates the directory in the Gopath', () => {
-			if (process.env.GOPATH) {
-				assert(fs.existsSync(appPath));
-			}
-		});
-	});
+  describe('Go file structure', () => {
+    it('generates the directory in the Gopath', () => {
+      if (process.env.GOPATH) {
+        assert(fs.existsSync(appPath));
+      }
+    });
+  });
 });
